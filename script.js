0e0d07b5ebcd5f320a43d29cb59783c35b7f0367
dylemat <ppoczesny@gmail.com>
2017-03-06 16:07:55 +0000

    poprawiony warunek wyświetlania tweeta - -nic powyzej 140 znakow sie nie wyswietli

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
-function getQuote() {
-	$.getJSON(quoteUrl, createTweet);
-}
-function createTweet(input) {
-	var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
-    	if (tweetText.length > 140) {
-	getQuote();
-}
-	if (!input.quoteAuthor.length) {
-		input.quoteAuthor = "Unknown author";
-	}
 
-else {
-	var tweet = tweetLink + encodeURIComponent(tweetText);
-	$('.quote').text(input.quoteText);
-	$('.author').text("Author: " + input.quoteAuthor);
-	$('.tweet').attr('href', tweet);
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
 }
+
+function createTweet(input) {
+    var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+    if (tweetText.length > 140) {
+        getQuote();
+    }
+
+
+
+
+    if (!input.quoteAuthor.length) {
+        input.quoteAuthor = "Unknown author";
+    }
+	var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(input.quoteText);
+    $('.author').text("Author: " + input.quoteAuthor);
+    $('.tweet').attr('href', tweet);
+    var tweet = tweetLink + encodeURIComponent(tweetText);
 }
 $(document).ready(function() {
-	getQuote();
-	$('.trigger').click(function() {
-		getQuote();
-	})
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
 });
